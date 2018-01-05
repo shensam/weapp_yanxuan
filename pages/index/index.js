3d97469b051b38dd954e14e43eab03b053ee72f9
qiuming <qiu_ming@hunliji.com>
2018-01-05 18:46:53 +0800

    添加页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,21 @@
 Page({
     data:{
+        "nav_bar_list":['推荐','居家','餐厨','配件','服装','电器','洗护','杂货','饮食','婴童','志趣'],
+        "nav_active_item":0,
+        "swiper_list":[]
+    },
+    handleNavTap:function(e) {
+        console.log(e)
+        let index = e.currentTarget.dataset.idx;
+        let query = wx.createSelectorQuery();
+        this.setData({
+            nav_active_item:index
+        })
+        query.select('#nav_'+index).boundingClientRect();
+        query.exec(function(res) {
+            let navW = res[0].width;
+            let totalW = (navW) * index
+            console.log(totalW)
+        })
     }
 })
