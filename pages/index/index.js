a92c12b9a3297c858e42922d9e5a1fc4c6081b9a
qiuming <qiu_ming@hunliji.com>
2018-01-08 17:47:34 +0800

    添加croll-view

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,13 @@ Page({
     data:{
         "nav_bar_list":['推荐','居家','餐厨','配件','服装','电器','洗护','杂货','饮食','婴童','志趣'],
         "nav_active_item":0,
-        "swiper_list":[]
+        "swiper_list":[
+            'http://yanxuan.nosdn.127.net/e5891a68df93c5ee57ddb5b256068dac.jpg?imageView&thumbnail=750x0&quality=75',
+            'http://yanxuan.nosdn.127.net/291878253cb96c6992c8a5c875248b4d.jpg?imageView&thumbnail=750x0&quality=75',
+            'http://yanxuan.nosdn.127.net/c0cf7b4cf7dcbc6dd9866ab2d23faf93.jpg?imageView&thumbnail=750x0&quality=75',
+            'http://yanxuan.nosdn.127.net/046d56ac0cd8adadf368fb8fd478c8c8.jpg?imageView&thumbnail=750x0&quality=75',
+            'http://yanxuan.nosdn.127.net/7ae187def725a14c75ae78d4b2f8e14a.jpg?imageView&thumbnail=750x0&quality=75'
+        ],
     },
     handleNavTap:function(e) {
         console.log(e)
